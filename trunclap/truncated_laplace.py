f2209beabd9dd22244e0665a3c4ece44fa7c4567
rhrhhrhr <hrhit421@foxmail.com>
2024-03-13 16:10:51 +0800

    实验代码及数据

--- trunclap/truncated_laplace.py
+++ trunclap/truncated_laplace.py
@@ -1,5 +1,4 @@
 import numpy as np
-import warnings
 
 
 class TruncatedLaplaceError(Exception):
@@ -22,55 +21,56 @@ class TruncatedLaplace(object):
         self.__pdf_max = self.get_pdf_max()
 
     @property
-    def low(self):
+    def low(self) -> int or float:
         return self.__low
 
     @low.setter
-    def low(self, value: int or float):
-        if value <= self.__high:
-            self.__low = value
-            self.__k = self.get_k()
-            self.__pdf_max = self.get_pdf_max()
-        else:
-            warnings.warn('Can\'t set a lower bound greater than the upper bound')
+    def low(self, value: int or float) -> None:
+        if value > self.__high:
+            raise TruncatedLaplaceError('Can\'t set a lower bound greater than the upper bound')
+
+        self.__low = value
+        self.__k = self.get_k()
+        self.__pdf_max = self.get_pdf_max()
 
     @property
-    def high(self):
+    def high(self) -> int or float:
         return self.__high
 
     @high.setter
-    def high(self, value: int or float):
-        if value >= self.__low:
-            self.__high = value
-            self.__k = self.get_k()
-            self.__pdf_max = self.get_pdf_max()
-        else:
-            warnings.warn('Can\'t set a upper bound less than the lower bound')
+    def high(self, value: int or float) -> None:
+        if value < self.__low:
+            raise TruncatedLaplaceError('Can\'t set a upper bound less than the lower bound')
+
+        self.__high = value
+        self.__k = self.get_k()
+        self.__pdf_max = self.get_pdf_max()
 
     @property
-    def location(self):
+    def location(self) -> int or float:
         return self.__location
 
     @location.setter
-    def location(self, value: int or float):
+    def location(self, value: int or float) -> None:
         self.__location = value
         self.__k = self.get_k()
         self.__pdf_max = self.get_pdf_max()
 
     @property
-    def scale(self):
+    def scale(self) -> int or float:
         return self.__scale
 
     @scale.setter
-    def scale(self, value: int or float):
-        if value > 0:
-            self.__scale = value
-            self.__k = self.get_k()
-            self.__pdf_max = self.get_pdf_max()
-        else:
-            warnings.warn('The scale must be positive!')
+    def scale(self, value: int or float) -> None:
+        if value <= 0:
+            raise TruncatedLaplaceError('The scale must be positive!')
 
-    def get_k(self):
+        self.__scale = value
+        self.__k = self.get_k()
+        self.__pdf_max = self.get_pdf_max()
+
+    # Get the probability of a Laplacian random variable belonging to interval [low, high]
+    def get_k(self) -> int or float:
         high_diff = self.__high - self.__location
         low_diff = self.__low - self.__location
 
@@ -79,16 +79,19 @@ class TruncatedLaplace(object):
 
         return (high_term - low_term) / 2
 
-    def pdf(self, u):
+    # Probability Density Function
+    def pdf(self, u) -> int or float:
         return np.exp(-np.abs(u - self.__location) / self.__scale) / (2 * self.__scale * self.__k)
 
-    def get_pdf_max(self):
+    def get_pdf_max(self) -> int or float:
         val = max(self.__low, min(self.__high, self.__location))
         pdf_max = self.pdf(val)
 
         return pdf_max
 
-    def get_random_scalar_value(self, var=None):
+    def get_random_scalar_value(self) -> int or float:
+        # Acceptance-Rejection Method
+
         while True:
             rand1 = np.random.uniform(self.__low, self.__high)
             rand2 = np.random.uniform(0, self.__pdf_max)
@@ -98,7 +101,7 @@ class TruncatedLaplace(object):
 
         return rand1
 
-    def __call__(self, dim=1):
+    def __call__(self, dim=1) -> int or float or np.ndarray:
         if not isinstance(dim, int) and dim >= 0:
             raise TruncatedLaplaceError("The parameter size must be positive integers.")
 
